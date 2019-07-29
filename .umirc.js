9202bce50cd7897aa7724af07a74f8c20e8b330b
ycjcl868 <45808948@qq.com>
2019-07-29 16:51:33 +0800

    feat: dynamic prerender

--- .umirc.js
+++ .umirc.js
@@ -3,12 +3,16 @@ const isPage = process.env.GITHUB_PAGE === 'true';
 
 export default {
   ssr: true,
+  hash: isPage,
   disableGlobalVariables: true,
   publicPath: isPage ? '/umi-example-ssr/' : '/static/',
   base: isPage ? '/umi-example-ssr/' : '/',
   plugins: [
     ['umi-plugin-react', {
       dva: true,
+      dynamicImport: {
+        webpackChunkName: true,
+      },
       title: {
         defaultTitle: 'my app',
       },
