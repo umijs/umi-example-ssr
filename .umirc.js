4c856cc45e474d5fa504758654f8ae5e7f7f6f47
ycjcl868 <45808948@qq.com>
2019-08-14 08:28:40 +0800

    feat: config

--- .umirc.js
+++ .umirc.js
@@ -1,25 +1,18 @@
 const DocumentTitle = require('react-document-title');
 const isPage = process.env.GITHUB_PAGE === 'true';
 
-export default {
-  ssr: true,
-  hash: isPage,
-  disableGlobalVariables: true,
-  outputPath: './public',
-  publicPath: isPage ? '/umi-example-ssr/' : '/public/',
-  base: isPage ? '/umi-example-ssr/' : '/',
-  plugins: [
-    ['umi-plugin-react', {
-      dva: true,
-      dynamicImport: {
-        webpackChunkName: true,
-      },
-      title: {
-        defaultTitle: 'my app',
-      },
-    }],
-    ['umi-plugin-gh-pages'],
-    ...(isPage ? ['@umijs/plugin-prerender', {
+const plugins = [
+  ['umi-plugin-react', {
+    dva: true,
+    dynamicImport: {
+      webpackChunkName: true,
+    },
+    title: {
+      defaultTitle: 'my app',
+    },
+  }],
+  ['umi-plugin-gh-pages'],
+  ...(isPage ? [['@umijs/plugin-prerender', {
       postProcessHtml: ($, path) => {
         const title = DocumentTitle.rewind();
 
@@ -29,6 +22,15 @@ export default {
 
         return $;
       }
-    }] : []),
-  ],
+  }]] : []),
+]
+
+export default {
+  ssr: true,
+  hash: isPage,
+  disableGlobalVariables: true,
+  outputPath: './public',
+  publicPath: isPage ? '/umi-example-ssr/' : '/public/',
+  base: isPage ? '/umi-example-ssr/' : '/',
+  plugins,
 };
